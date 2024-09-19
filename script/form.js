a90d32f0f45876c4d7e2ad3afa9aae20f0792af2
TheSquiddyLink <preston@happychaps.com>
2024-09-19 11:43:39 -0700

    Begin Form Image

--- script/form.js
+++ script/form.js
@@ -1,25 +1,4 @@
-document.getElementById("form").addEventListener("change", formChange);
-
-/**
- * 
- * @param {Event} event 
- */
-function formChange(event){
-    console.log('Form Changing')
-    console.log(event);
-
-    var totalElement = document.getElementById("total");
-
-    var data = new FormData(event.currentTarget);
-
-    totalElement.innerHTML = data.total;
-    
-
-}
-
 class FormData {
-
-
     name;
     /**
      * @type {HTMLSelectElement}
@@ -60,6 +39,11 @@ class FormData {
         }
     }
 
+    get packageName(){
+        return this.package.options[this.package.selectedIndex].text
+    }
+    
+
     /**
      * @param {HTMLSelectElement} selectElement
      * @private
@@ -67,4 +51,49 @@ class FormData {
     selectedCost(selectElement){
         return Number.parseInt(selectElement.options[selectElement.selectedIndex].getAttribute("cost"))
     }
+}
+
+document.getElementById("form").addEventListener("change", generateImage);
+
+
+/**
+ * 
+ * @param {Event} event 
+ */
+async function generateImage(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    let formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    const canvas = document.getElementById("canvas");
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext("2d");
+
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+
+    console.log(canvasWidth, canvasHeight);
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // Text
+    ctx.font = "50px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(formData.name.value, 10, 50);
+
+    ctx.font = "20px Arial";
+    ctx.fillText(formData.packageName, 10, canvasHeight/2-10);
+
+
+    ctx.font = "20px Arial";
+    ctx.fillText("Extra Characters - " + formData.additions.extraCharacters.value, 10, canvasHeight-30);
+
+    ctx.font = "20px Arial";
+    ctx.fillText("Background - " + formData.additions.background.value, 10, canvasHeight-10);
+
+    ctx.font = "50px Arial";
+    ctx.fillText("$"+formData.total, canvasWidth-100, canvasHeight/2+10);
 }
