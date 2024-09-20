0e7e73c4f3fec97f5c1e3b7d6f3eb4a2b158b832
TheSquiddyLink <preston@happychaps.com>
2024-09-20 16:20:01 -0700

    Swapping Style

---
+++ script/main.js
@@ -0,0 +1,15 @@
+function swap(pkg){
+    const img1 = document.getElementById(pkg+"-img-1");
+    const img2 = document.getElementById(pkg+"-img-2");
+    const type = document.getElementById(pkg+"-type")
+
+    if(img1.hidden){
+        type.innerHTML = img1.getAttribute("type");
+        img1.hidden = false;
+        img2.hidden = true;
+    }else {
+        type.innerHTML = img2.getAttribute("type");
+        img1.hidden = true;
+        img2.hidden = false;
+    }
+}
